98b59be92182ba42e1c569411108e0469e84ac33
uzairdurani <muhammaduzairdurani@gmail.com>
2022-03-29 11:58:06 -0700

    thyroid detection using multiple models

--- LregressionModel1/forms.py
+++ LregressionModel1/forms.py
@@ -4,66 +4,53 @@ from django.forms import fields
 
 class newForm(forms.Form):
 
-    CRIM = forms.FloatField(
-
-
-        label='CRIM',
-        widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'CRIM', 'id': 'form-CRIM'}),
-
-
-    )
-
-    INDUS = forms.FloatField(
-
-        label='INDUS', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'INDUS', 'id': 'form-INDUS'}))
-
-    CHAS = forms.FloatField(
-
-        label='CHAS', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'CHAS', 'id': 'form-CHAS'}))
-
-    NOX = forms.FloatField(
-
-        label='NOX', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'NOX', 'id': 'form-NOX'}))
-
-    RM = forms.FloatField(
-
-        label='RM', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'RM', 'id': 'form-RM'}))
-
     AGE = forms.FloatField(
 
         label='AGE', widget=forms.TextInput(
             attrs={'class': 'form-control mb-3', 'placeholder': 'AGE', 'id': 'form-AGE'}))
-    DIS = forms.FloatField(
-        label='DIS', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'DIS', 'id': 'form-DIS'}))
 
-    RAD = forms.FloatField(
-        label='RAD', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'RAD', 'id': 'form-RAD'}))
-    TAX = forms.FloatField(
-        label='TAX', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'TAX', 'id': 'form-TAX'}))
-    PTRATIO = forms.FloatField(
-        label='PTRATIO', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'PTRATIO', 'id': 'form-PTRATIO'}))
-    B = forms.FloatField(
-        label='B', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'B', 'id': 'form-B'}))
-    LSTAT = forms.FloatField(
-        label='LSTAT', widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'LSTAT', 'id': 'form-LSTAT'}))
+    TSH = forms.FloatField(
 
-    # choices = (
-    #     (1, 'yes'),
-    #     (0, 'no')
-    # )
-    # Research = forms.ChoiceField(choices=choices, widget=forms.Select(
-    #     attrs={'class': 'form-control mb-3', 'id': 'form_research'}))
+
+        label='TSH',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'TSH', 'id': 'form-TSG'}),
+
+
+    )
+
+    T3 = forms.FloatField(
+
+        label='T3', widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'T3', 'id': 'form-T3'}))
+
+    TT4 = forms.FloatField(
+
+        label='TT4', widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'TT4', 'id': 'form-TT4'}))
+
+    T4U = forms.FloatField(
+
+        label='T4U', widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'T4U', 'id': 'form-T4U'}))
+
+    FTI = forms.FloatField(
+
+        label='FTI', widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'FTI', 'id': 'form-FTI'}))
+
+    choices = (
+        (1, 'Surgery (Yes)'),
+        (0, 'Surgery (No)'),
+    )
+    SURGERY = forms.ChoiceField(choices=choices, widget=forms.Select(
+        attrs={'class': 'form-control mb-3', 'id': 'form_SURGERY'}))
+    choices = (
+        (1, 'Male'),
+        (0, 'Female')
+    )
+    SEX = forms.ChoiceField(choices=choices, widget=forms.Select(
+        attrs={'class': 'form-control mb-3', 'id': 'form_SEX'}))
 
     class Meta:
         fields = {'GREScore'}
--- LregressionModel1/views.py
+++ LregressionModel1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect, render
 from .forms import newForm
 from django.template.defaulttags import register
 import pickle
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 
 def home(request):
@@ -12,8 +12,10 @@ def home(request):
     return render(request, 'home.html', {'form': form})
 
 
-reg_model = pickle.load(open('BostonModel.pickle', 'rb'))
-scaler = pickle.load(open('BostonScalerModel.pickle', 'rb'))
+reg_model = pickle.load(open('Logistic Regression.pickle', 'rb'))
+svm_model = pickle.load(open('Support Vector Machine.pickle', 'rb'))
+rf_model = pickle.load(open('Random Forest.pickle', 'rb'))
+scaler = pickle.load(open('scalermodel.pickle', 'rb'))
 
 
 def predict(request):
@@ -21,27 +23,49 @@ def predict(request):
     if request.method == 'POST':
         form = newForm(request.POST)
         if form.is_valid():
-            CRIM = form.cleaned_data['CRIM']
-            INDUS = form.cleaned_data['INDUS']
-            CHAS = form.cleaned_data['CHAS']
-            NOX = form.cleaned_data['NOX']
-            RM = form.cleaned_data['RM']
             AGE = form.cleaned_data['AGE']
-            DIS = form.cleaned_data['DIS']
+            TSH = form.cleaned_data['TSH']
+            T3 = form.cleaned_data['T3']
+            TT4 = form.cleaned_data['TT4']
+            T4U = form.cleaned_data['T4U']
+            FTI = form.cleaned_data['FTI']
 
-            RAD = form.cleaned_data['RAD']
-            TAX = form.cleaned_data['TAX']
-            PTRATIO = form.cleaned_data['PTRATIO']
-            LSTAT = form.cleaned_data['LSTAT']
-            B = form.cleaned_data['B']
+            SURGERY = int(form.cleaned_data['SURGERY'])
 
-            data = [CRIM, INDUS, CHAS, NOX, RM, AGE,
-                    DIS, RAD, TAX, PTRATIO, B, LSTAT]
-            data = scaler.transform([data])
-            print(data)
-            prediction = format(reg_model.predict(data)[0], '.2f')
+            SEX = int(form.cleaned_data['SEX'])
 
-            return render(request, 'home.html', {'form': form, 'prediction': prediction})
+            # data = [AGE, TT4, FTI]
+            AGE = scaler.transform([[AGE]])[0][0]
+            TT4 = scaler.transform([[TT4]])[0][0]
+            FTI = scaler.transform([[FTI]])[0][0]
+
+            # SURGERY = LabelEncoder().fit_transform([SURGERY])
+            # SEX = LabelEncoder().fit_transform([SEX])
+            # print(SURGERY, SEX)
+
+            data = [AGE, SEX, SURGERY, TSH, T3, TT4, T4U, FTI]
+            # prediction = format(reg_model.predict(data)[0], '.2f')
+
+            prediction = reg_model.predict([data])[0]
+            prediction_svm = svm_model.predict([data])[0]
+            prediction_rf = rf_model.predict([data])[0]
+
+            # print(prediction[0], prediction_svm[0], prediction_rf[0])
+            predictions = [prediction, prediction_svm, prediction_rf]
+            count_1 = 0
+            count_0 = 0
+            for i in range(len(predictions)):
+                if predictions[i] == 1:
+                    count_1 += 1
+                else:
+                    count_0 += 1
+            final_prediction = None
+            if count_1 > count_0:
+                final_prediction = 'Postive'
+            else:
+                final_prediction = "Negative"
+            print(predictions, final_prediction)
+            return render(request, 'home.html', {'form': form, 'finalPrediction': final_prediction, 'predictions': predictions})
         else:
             error = form.errors.as_data()
             print(error)
